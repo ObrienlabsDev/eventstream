5424e398f6e6abefb92e4500a2448830dfbc2557
Michael O'Brien <michael@obrienlabs.org>
2023-12-16 08:41:26 -0500

    #4 - spring boot initial template

---
+++ src/main/java/dev/obrienlabs/eventstream/EventstreamApplication.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.eventstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventstreamApplication.class, args);
+	}
+
+}
