c37d584cecef0cc39c528116c61e545a9500dac7
Michael O'Brien <michael@obrienlabs.org>
2024-05-20 14:22:49 -0400

    #4 - gcp serverless script

--- eventstream-nbi/src/deployment.sh
+++ eventstream-nbi/src/deployment.sh
@@ -19,11 +19,35 @@ deployment() {
   echo "Date: $(date)"
   echo "Timestamp: $(date +%s)"
   echo "$UNIQUE"
-  echo "running with create=${CREATE_PROJ} delete=${DELETE_PROJ}"
-
+  echo "running with create=${CREATE_PROJ} delete=${DELETE_PROJ} boot_project_id=${BOOT_PROJECT_ID}"
 
 if [[ "$CREATE_PROJ" != false ]]; then
-  # 
+  # linux
+  STREAM_PROJECT_RAND=$(shuf -i 0-10000 -n 1)
+  # osx
+  #STREAM_PROJECT_RAND=$(jot -r 1 1000 10000)
+  STREAM_PROJECT_ID=${STREAM_PROJECT_NAME_PREFIX}-${STREAM_PROJECT_RAND}
+  echo "Creating project: $STREAM_PROJECT_ID"
+else
+  STREAM_PROJECT_ID=${STREAM_PROJECT_ID_PASSED}
+  echo "Reusing project: $STREAM_PROJECT_ID"
+fi
+
+echo "STREAM_PROJECT_ID: $STREAM_PROJECT_ID"
+exit 1
+
+if [[ "$CREATE_PROJ" != false ]]; then
+  # create project
+  gcloud config set project "${BOOT_PROJECT_ID}"
+  BILLING_FORMAT="--format=value(billingAccountName)"
+  BILLING_ID=$(gcloud billing projects describe $BOOT_PROJECT_ID $BILLING_FORMAT | sed 's/.*\///')
+  gcloud beta billing projects link "${STREAM_PROJECT_ID}" --billing-account "${BILLING_ID}"
+  ORG_ID=$(gcloud projects get-ancestors $BOOT_PROJECT_ID --format='get(id)' | tail -1)
+  
+  gcloud beta billing projects link "${STREAM_PROJECT_ID}" --billing-account "${BILLING_ID}"  
+  gcloud config set project "${STREAM_PROJECT_ID}"
+
+  
   # service account
 
   # iam roles for user
@@ -39,7 +63,9 @@ if [[ "$CREATE_PROJ" != false ]]; then
   # BQ schema
   
   # Logging
-   
+ 
+  # RETURN
+  gcloud config set project "${BOOT_PROJECT_ID}"  
 
 
 
@@ -60,13 +86,18 @@ fi
 UNIQUE=old
 CREATE_PROJ=false
 DELETE_PROJ=false
-while getopts ":c:d:u:" PARAM; do
+STREAM_PROJECT_ID=
+BOOT_PROJECT_ID=
+while getopts ":c:d:b:u:" PARAM; do
   case $PARAM in
     c)
       CREATE_PROJ=${OPTARG}
       ;;
     d)
       DELETE_PROJ=${OPTARG}
+      ;;
+    b)
+      BOOT_PROJECT_ID=${OPTARG}
       ;;
     ?)
       usage
@@ -75,7 +106,7 @@ while getopts ":c:d:u:" PARAM; do
   esac
 done
 
-#  echo "Options are: -c true/false (create) -d true/false (delete proj)"
+#  echo "Options are: -c true/false (create) -d true/false (delete proj) -b BOOT_PROJ_ID"
 
 
 if [[ -z $UNIQUE ]]; then
@@ -84,5 +115,5 @@ if [[ -z $UNIQUE ]]; then
 fi
 
 echo "existing project: $PROJECT_ID"
-deployment "$CREATE_PROJ" "$DELETE_PROJ"
+deployment "$CREATE_PROJ" "$DELETE_PROJ" "$BOOT_PROJECT_ID"
 printf "**** Done ****\n"
--- eventstream-nbi/src/vars.sh
+++ eventstream-nbi/src/vars.sh
@@ -4,3 +4,4 @@ export PROJECT_NAME=eventstream-dev
 export REGION=us-central1
 export SUPER_ADMIN_EMAIL=michael@obrienlabs.dev
 
+export STREAM_PROJECT_NAME_PREFIX=stream-old
